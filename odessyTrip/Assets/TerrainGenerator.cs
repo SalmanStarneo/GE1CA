090450af90b50ce6e77fec236490bbd71c28b2b7
SalmanStarneo <salman.alsamiri@gmail.com>
2021-12-16 15:59:25 +0000

    Intial Commit Setting, changing unity version from 2020 to 2019 due to unity.collab-proxy/Editor/PlaticSCM/.... issue of refusing to upload to github due to naming and caching issues-Ver3

---
+++ odessyTrip/Assets/TerrainGenerator.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+
+public class TerrainGenerator : MonoBehaviour
+{
+
+    public int depth = 20;
+
+    public int width = 256;
+    public int height = 256;
+
+    public float scale = 20f;
+
+    public float offsetX = 100f;
+    public float offsetY = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offsetX=Random.Range(0f,9999f);
+        offsetY=Random.Range(0f,9999f);
+        
+    }
+
+    
+     // Update is called once per frame
+    void Update()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData= GenerateTerrain(terrain.terrainData);
+        offsetX+=Time.deltaTime*5f;
+    }
+
+    TerrainData GenerateTerrain (TerrainData terrainData)
+    {
+        terrainData.heightmapResolution = width + 1;
+
+        terrainData.size = new Vector3(width, depth, height);
+
+        terrainData.SetHeights(0,0,GenerateHeights());
+
+        return terrainData;
+    }
+
+    float[,] GenerateHeights ()
+    {
+        float[,] heights = new float[width, height];
+        for(int x = 0 ; x<width ; x++)
+        {
+            for(int y = 0 ; y < height ; y++)
+            {
+                heights[x,y]=CalculateHeight(x,y);
+            }
+        }
+
+        return heights;
+    }
+
+    float CalculateHeight (int x, int y)
+    {
+        float xCoord =(float) x / width * scale + offsetX;
+        float yCoord =(float)  y/ height * scale + offsetY;
+
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+   
+}
