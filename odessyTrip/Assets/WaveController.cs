c8698815b17c8e2fd6804a1860415b3fd802690e
SalmanStarneo <salman.alsamiri@gmail.com>
2021-12-19 12:37:53 +0000

    Alteration to the Sky and Terrain by using shaders

---
+++ odessyTrip/Assets/WaveController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour
+{
+
+    public float height;
+    public float time;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        iTween.MoveBy(this.gameObject, iTween.Hash("y",height,"time",time,"looptype","pingpong","easeType",iTween.EaseType.easeInOutSine));
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
